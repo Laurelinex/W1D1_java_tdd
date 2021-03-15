fb3925c06c58954e0629e1f5911ac393767d7ed6
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-15 15:04:04 +0000

    Water bottle class set up + 1st test passed

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,15 @@
+public class WaterBottle {
+    private double volume;
+
+    public WaterBottle(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+}
