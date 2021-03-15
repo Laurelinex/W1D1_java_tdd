183e486e70e5307e22bef6edc78e3ceebc255a2f
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-15 15:29:28 +0000

    Extension started - Printer class set up + 1st test pass

---
+++ src/main/java/Printer.java
@@ -0,0 +1,15 @@
+public class Printer {
+    private int sheetsOfPaper;
+
+    public Printer(int sheetsOfPaper) {
+        this.sheetsOfPaper = sheetsOfPaper;
+    }
+
+    public int getSheetsOfPaper() {
+        return sheetsOfPaper;
+    }
+
+    public void setSheetsOfPaper(int sheetsOfPaper) {
+        this.sheetsOfPaper = sheetsOfPaper;
+    }
+}
